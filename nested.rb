494a2812f0c9160ee31fa73456c6d35dfaf501f3
Boonie Attawut <puifai.ba@gmail.com>
2020-12-16 05:26:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dinnis_ritchie][:languages]
+programmer_hash[:dinnis_ritchie][:languages][]
 end
 
 def adding_matz
